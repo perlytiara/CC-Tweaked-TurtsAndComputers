33ff0fef43480072ee6f7bd0a41e393a84685a9b
perlytiara <overflowedimagination@gmail.com>
2025-09-18 13:01:54 +0200

    added Maengorn

--- programs/Maengorn/startup.lua
+++ programs/Maengorn/startup.lua
@@ -1,10 +1,18 @@
 -- Always copy the clientdig program from disk, overwriting if it exists
+print("Starting turtle setup...")
+
 if fs.exists("/clientdig") then
+    print("Removing existing clientdig...")
     fs.delete("/clientdig")
 end
-fs.copy("disk/clientdig", "/clientdig")
 
--- Run the clientdig program
-shell.run("clientdig")
+if fs.exists("disk/clientdig") then
+    print("Copying clientdig from disk...")
+    fs.copy("disk/clientdig", "/clientdig")
+    print("Running clientdig program...")
+    shell.run("clientdig")
+else
+    print("ERROR: disk/clientdig not found!")
+end
     
     
