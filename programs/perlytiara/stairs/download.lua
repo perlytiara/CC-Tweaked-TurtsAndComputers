b470f99df3cacaf954f70473f15ad24547c49704
perlytiara <overflowedimagination@gmail.com>
2025-09-27 13:59:42 +0200

    feat: Add eSlicer - Enhanced mining system with paired turtle coordination

--- programs/perlytiara/stairs/download.lua
+++ programs/perlytiara/stairs/download.lua
@@ -8,26 +8,17 @@ local files = {
   startup = "https://raw.githubusercontent.com/perlytiara/CC-Tweaked-TurtsAndComputers/refs/heads/main/programs/perlytiara/stairs/startup.lua"
 }
 
--- For now, just copy from local files since we don't have URLs
-local function copyFile(from, to)
-  local source = fs.open(from, "r")
-  if not source then
-    print("Failed to read " .. from)
+-- Download file using wget
+local function downloadFile(url, filename)
+  print("Downloading " .. filename .. "...")
+  local result = shell.run("wget", url, filename)
+  if result then
+    print("✓ " .. filename)
+    return true
+  else
+    print("✗ Failed to download " .. filename)
     return false
   end
-  
-  local content = source.readAll()
-  source.close()
-  
-  local dest = fs.open(to, "w")
-  if not dest then
-    print("Failed to write " .. to)
-    return false
-  end
-  
-  dest.write(content)
-  dest.close()
-  return true
 end
 
 -- Create programs directory if it doesn't exist
@@ -35,27 +26,24 @@ if not fs.exists("programs") then
   fs.makeDir("programs")
 end
 
--- Copy files to programs directory
+-- Download files to programs directory
 local success = 0
-if copyFile("stairs.lua", "programs/stairs") then
-  print("✓ stairs")
+
+if downloadFile(files.stairs, "programs/stairs") then
   success = success + 1
 end
 
-if copyFile("client.lua", "programs/client") then
-  print("✓ client")  
+if downloadFile(files.client, "programs/client") then
   success = success + 1
 end
 
-if copyFile("multi.lua", "programs/multi") then
-  print("✓ multi")
+if downloadFile(files.multi, "programs/multi") then
   success = success + 1
 end
 
 -- Setup startup file for turtles
 if turtle then
-  if copyFile("startup.lua", "startup") then
-    print("✓ startup (turtle)")
+  if downloadFile(files.startup, "startup") then
     success = success + 1
   end
 else
