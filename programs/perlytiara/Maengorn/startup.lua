f3e986ebfc41d3d9c4cabb061abd02bc843ab14c
perlytiara <overflowedimagination@gmail.com>
2025-10-01 09:26:49 +0200

    Update to latest

--- programs/perlytiara/Maengorn/startup.lua
+++ programs/perlytiara/Maengorn/startup.lua
@@ -1,7 +1,23 @@
-if not fs.exists("/startup") then
-    fs.copy("disk/clientdig", "/startup")
-    fs.copy("disk/clientdig", "/clientdig")
-    shell.run("clientdig")
+-- Startup script for newly deployed turtles
+if not fs.exists("/clientdig") then
+    -- First boot - copy files from disk
+    if fs.exists("disk/clientdig") then
+        fs.copy("disk/clientdig", "/clientdig")
+        print("Copied clientdig from disk")
+    else
+        print("ERROR: No clientdig found on disk!")
+        return
     end
     
+    if fs.exists("disk/startup") then
+        fs.copy("disk/startup", "/startup")
+        print("Copied startup from disk")
+    end
     
+    print("Setup complete, rebooting...")
+    os.sleep(1)
+    os.reboot()
+else
+    -- Files already installed, run the client
+    shell.run("clientdig")
+end
